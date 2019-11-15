79f4e9228fd6de64b809d0a32944f96e7e2017d3
Ishtyaq Ponir <ishtyaqponir@gmail.com>
2019-11-14 20:22:28 -0500

    Updated Google and Facebook Auth, need to debug sign-out function

--- js/index.js
+++ js/index.js
@@ -19,30 +19,57 @@ function googleLogin() {
     //TODO: Add functionality
 }
 
+function switchPage() {
+    window.location.href="main.html";
+}
 function onSignIn(googleUser) {
 
-    try { 
-        var profile = googleUser.getBasicProfile();
-        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-        console.log('Name: ' + profile.getName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    switchPage();
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    
+    var myUserEntity = {};
+    myUserEntity.Id = profile.getId();
+    myUserEntity.Name = profile.getName();
+    
+    //Store the entity object in sessionStorage where it will be accessible from all pages of your site.
+    sessionStorage.setItem('myUserEntity',JSON.stringify(myUserEntity));
+}
 
-        window.open("secIndex.html",'_self');
-
-    } catch(error) {
-        console.log(error);
-    }
+function checkIfLoggedIn()
+{
+  if(sessionStorage.getItem('myUserEntity') == null){
+    //Redirect to login page, no user entity available in sessionStorage
+    window.location.href='main.html';
+  } else {
+    //User already logged in
+    var userEntity = {};
+    userEntity = JSON.parse(sessionStorage.getItem('myUserEntity'));
+    window.location.href='main.html';
+  }
 }
 
 function signOut() {
+
+    //window.location.href = "index.html";
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
     });
   }
 
+  function onLoad() {
+    gapi.load('auth2', function() {
+      gapi.auth2.init();
+    });
+  }
+
 function checkLoginState() {
+
+    window.location.href = "main.html";
     FB.getLoginStatus(function(response) {
       statusChangeCallback(response);
     });
@@ -62,6 +89,4 @@ function appyLogin() {
     client.on('connect', function() {
         console.log('connected');
     })
-
-
 }
