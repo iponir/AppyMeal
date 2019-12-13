a8a34c105beaba7b0bcc9195be742756e7321ad1
Ishtyaq Ponir <ishtyaqponir@gmail.com>
2019-12-12 20:44:38 -0500

    Added require.js config modules

--- js/config.js
+++ js/config.js
@@ -1,8 +1,9 @@
 //Config File
-//Can add CDMs [Online script] before local copy of libraries 
+//Can add CDNs [Online script] before local copy of libraries 
 requirejs.config({
     baseUrl:'js',
     paths:{
+        redis: '',
         angular:'angular.min',
         extCore:'ext-core',
         jquery:'jquiery.min',
