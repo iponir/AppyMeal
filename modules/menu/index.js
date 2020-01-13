d6feebfea97066c1d8eb3c8fbcce3db6bc9ec18b
Madison Hanberry <mhanberry1@gmail.com>
2020-01-12 23:41:11 -0500

    added menu

---
+++ modules/menu/index.js
@@ -0,0 +1,4 @@
+// Toggle the active state of the specified element
+function toggleActive(element){
+	element.classList.contains("active") ? element.classList.remove("active") : element.classList.add("active");
+}
